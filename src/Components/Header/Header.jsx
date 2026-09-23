17bc138ed04432ee823b8a944a8fca085992245e
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 16:01:08 +0500

    libk qoshildi

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <div className="header">
+        <div className="container">
+            <div className="header__container">
+                <h1 className='header__title'>Flower Shop</h1>
+                <ul className='header__list'>
+                    <li className='header__item'>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <Link to="/shop">Shop</Link>
+                        <Link to="/blog">Blog</Link>
+                    </li>
+                    <li className='header__item'></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
