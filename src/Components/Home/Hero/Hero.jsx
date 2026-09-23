ae4afe71f31370ddb9a1e20148a966eacef1ea40
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 16:52:09 +0500

    rasm yuklandi

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import hero1 from '../Images/hero1.png'
 
 const Hero = () => {
   return (
@@ -6,7 +7,16 @@ const Hero = () => {
     <div className="hero">
         <div className="container">
             <div className="hero__container">
-                <h1 className=''></h1>
+                <ul className='hero__list'>
+                    <li className='hero__item'>
+                        <h1 className='hero__title'>Flowers, 🌻 what the world needs</h1>
+                        <p className='hero__text'>Browse between hounders of flowers</p>
+                        <button className='hero__btn'>Browse</button>
+                    </li>
+                    <li className='hero__item'>
+                        <img src={hero1} alt="" />
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
