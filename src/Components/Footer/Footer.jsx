f89daae9a2cd707c5434f73bdef33f6a38cf75e5
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 16:19:13 +0500

    footerga matn qoshildi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import './Footer.css'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer className='footer'>
+        <div className="container">
+            <div className="footer__container">
+                <ul className='footer__list'>
+                    <li className='footer-item'>
+                        <h1 className='footer__title'>Flower Shop</h1>
+                        <p>
+                            Some random stuff about flower shop and 
+                            some more info cuz this box had to get fill 
+                            Some random stuff about flower shop and 
+                            some more info cuz this box had to get fill 
+                            Some random stuff about flower shop and 
+                            some more info cuz this box had to get fill
+                        </p>
+                    </li>
+                    <li className='footer-item'></li>
+                    <li className='footer-item'></li>
+                    <li className='footer-item'></li>
+                </ul>
+            </div>
+        </div>
+      </footer>
+    </>
   )
 }
 
