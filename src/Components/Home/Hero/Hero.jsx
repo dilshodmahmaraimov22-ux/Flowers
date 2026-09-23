db61b5b985dfd47c213f1f64beec296a00c11c27
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 16:46:40 +0500

    hero yaratildi

---
+++ src/Components/Home/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <>
+    <div className="hero">
+        <div className="container">
+            <div className="hero__container">
+                <h1 className=''></h1>
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
