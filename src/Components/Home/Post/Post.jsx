f951070a3ab72ffefc6a7a800a4350e3bf02ecc0
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 18:00:45 +0500

    avatar qoshildi

--- src/Components/Home/Post/Post.jsx
+++ src/Components/Home/Post/Post.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Post.css'
-
+import avatar from '../../Images/avatar1.jpg'
 
 const Post = () => {
   return (
@@ -8,10 +8,10 @@ const Post = () => {
     <div className="post">
         <div className="container">
             <div className="post__container">
-                <h1 className='post__title'></h1>
+                <h1 className='post__title'>Latest posts</h1>
                 <ul className='post__list'>
                     <li className='post__item'>
-                        <img className='post__avatar' src="" alt="avatar" />
+                        <img className='post__avatar' src={avatar} alt="avatar" />
                         <h2 className='post__date'></h2>
                         <img className='post__img' src="" alt="rasm" />
                         <h1 className='post__title'></h1>
@@ -20,7 +20,7 @@ const Post = () => {
                         <h1 className='post__title2'></h1>
                     </li>
                     <li className='post__item'>
-                        <img className='post__avatar' src="" alt="avatar" />
+                        <img className='post__avatar' src={avatar} alt="avatar" />
                         <h2 className='post__date'></h2>
                         <img className='post__img' src="" alt="rasm" />
                         <h1 className='post__title'></h1>
@@ -29,7 +29,7 @@ const Post = () => {
                         <h1 className='post__title2'></h1>
                     </li>
                     <li className='post__item'>
-                        <img className='post__avatar' src="" alt="avatar" />
+                        <img className='post__avatar' src={avatar} alt="avatar" />
                         <h2 className='post__date'></h2>
                         <img className='post__img' src="" alt="rasm" />
                         <h1 className='post__title'></h1>
