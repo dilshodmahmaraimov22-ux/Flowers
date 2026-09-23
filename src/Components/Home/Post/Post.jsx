73005977bf6dff34329502b55e187c7f10cebc13
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 17:55:23 +0500

    postga card qoshildi

--- src/Components/Home/Post/Post.jsx
+++ src/Components/Home/Post/Post.jsx
@@ -1,13 +1,43 @@
 import React from 'react'
 import './Post.css'
 
+
 const Post = () => {
   return (
     <>
     <div className="post">
         <div className="container">
             <div className="post__container">
-                
+                <h1 className='post__title'></h1>
+                <ul className='post__list'>
+                    <li className='post__item'>
+                        <img className='post__avatar' src="" alt="avatar" />
+                        <h2 className='post__date'></h2>
+                        <img className='post__img' src="" alt="rasm" />
+                        <h1 className='post__title'></h1>
+                        <p className='post__text'></p>
+                        <img className='post__like' src="" alt="like" />
+                        <h1 className='post__title2'></h1>
+                    </li>
+                    <li className='post__item'>
+                        <img className='post__avatar' src="" alt="avatar" />
+                        <h2 className='post__date'></h2>
+                        <img className='post__img' src="" alt="rasm" />
+                        <h1 className='post__title'></h1>
+                        <p className='post__text'></p>
+                        <img className='post__like' src="" alt="like" />
+                        <h1 className='post__title2'></h1>
+                    </li>
+                    <li className='post__item'>
+                        <img className='post__avatar' src="" alt="avatar" />
+                        <h2 className='post__date'></h2>
+                        <img className='post__img' src="" alt="rasm" />
+                        <h1 className='post__title'></h1>
+                        <p className='post__text'></p>
+                        <img className='post__like' src="" alt="like" />
+                        <h1 className='post__title2'></h1>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
