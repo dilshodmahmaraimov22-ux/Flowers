c9eb3dbccafec716c10e9a5991a96805a21684fb
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 16:32:19 +0500

    icon olib jkelindi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
+import { FaLocationArrow } from 'react-icons/fa'
+import { FaPhone } from 'react-icons/fa'
+import { FaAt } from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -20,9 +23,26 @@ const Footer = () => {
                             some more info cuz this box had to get fill
                         </p>
                     </li>
-                    <li className='footer-item'></li>
-                    <li className='footer-item'></li>
-                    <li className='footer-item'></li>
+                    <li className='footer-item'>
+                        <h1 className='footer__link1'>Links</h1>
+                        <Link className='footer__link'>Home</Link>
+                        <Link className='footer__link'>About</Link>
+                        <Link className='footer__link'>Shop</Link>
+                        <Link className='footer__link'>Login</Link>
+                    </li>
+                    <li className='footer-item'>
+                        <h1 className='footer__link1'>Links</h1>
+                        <Link className='footer__link'>Home</Link>
+                        <Link className='footer__link'>About</Link>
+                        <Link className='footer__link'>Shop</Link>
+                        <Link className='footer__link'>Login</Link>
+                    </li>
+                    <li className='footer-item'>
+                        <h1 className='footer_link1'>Contact</h1>
+                        <Link className='footer__link'> <FaLocationArrow/> 26985 Brighton Lane, Lake Forest, CA</Link>
+                        <Link className='footer__link'><FaAt/> support@Flowers.com</Link>
+                        <Link className='footer__link'> <FaPhone/> +1 236 5489</Link>
+                    </li>
                 </ul>
             </div>
         </div>
