222b183b9ff6aa98de6d5e94f300ff6fec2eee66
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 17:34:53 +0500

    post qoshildi

---
+++ src/Components/Home/Post/Post.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Post.css'
+
+const Post = () => {
+  return (
+    <>
+    <div className="post">
+        <div className="container">
+            <div className="post__container">
+                
+            </div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Post
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from '../../Components/Home/Hero/Hero'
 import Saler from '../../Components/Home/Saler/Saler'
+import Post from '../../Components/Home/Post/Post'
 
 const Home = () => {
   return (
     <>
     <Hero/>
     <Saler/>
+    <Post/>
     </>
   )
 }
