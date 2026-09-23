2f921f9d4cd965a36dc08078e93a712d7424d787
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 18:40:46 +0500

    footer tahrirlandi

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,53 +1,71 @@
 import React from 'react'
 import './Footer.css'
 import { Link } from 'react-router-dom'
-import { FaLocationArrow } from 'react-icons/fa'
-import { FaPhone } from 'react-icons/fa'
-import { FaAt } from 'react-icons/fa'
+import { FaLocationArrow, FaPhone, FaAt } from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <>
-      <footer className='footer'>
-        <div className="container">
-            <div className="footer__container">
-                <ul className='footer__list'>
-                    <li className='footer-item'>
-                        <h1 className='footer__title'>Flower Shop</h1>
-                        <p>
-                            Some random stuff about flower shop and <br />
-                            some more info cuz this box had to get fill <br />
-                            Some random stuff about flower shop and <br />
-                            some more info cuz this box had to get fill <br />
-                            Some random stuff about flower shop and <br />
-                            some more info cuz this box had to get fill <br />
-                        </p>
-                    </li>
-                    <li className='footer-item'>
-                        <h1 className='footer__link1'>Links</h1>
-                        <Link className='footer__link'>Home</Link>
-                        <Link className='footer__link'>About</Link>
-                        <Link className='footer__link'>Shop</Link>
-                        <Link className='footer__link'>Login</Link>
-                    </li>
-                    <li className='footer-item'>
-                        <h1 className='footer__link1'>Links</h1>
-                        <Link className='footer__link'>Home</Link>
-                        <Link className='footer__link'>About</Link>
-                        <Link className='footer__link'>Shop</Link>
-                        <Link className='footer__link'>Login</Link>
-                    </li>
-                    <li className='footer-item'>
-                        <h1 className='footer_link1'>Contact</h1>
-                        <Link className='footer__link'> <FaLocationArrow/> 26985 Brighton Lane, Lake Forest, CA</Link>
-                        <Link className='footer__link'><FaAt/> support@Flowers.com</Link>
-                        <Link className='footer__link'> <FaPhone/> +1 236 5489</Link>
-                    </li>
-                </ul>
-            </div>
+    <footer className='footer'>
+      <div className="container">
+        <div className="footer__container">
+          <ul className='footer__list'>
+            
+            {/* 1-Ustun: Shop haqida */}
+            <li className='footer-item footer-item--about'>
+              <h1 className='footer__title'>
+                Flower <span className="highlight">Shop</span>
+              </h1>
+              <p className='footer__desc'>
+                Some random stuff about flower shop and some more info cuz this box had to get fill. 
+                Some random stuff about flower shop and some more info cuz this box had to get fill.
+              </p>
+            </li>
+
+            {/* 2-Ustun: Links */}
+            <li className='footer-item'>
+              <h2 className='footer__subtitle'>Links</h2>
+              <div className="footer__links-group">
+                <Link to="/" className='footer__link'>Home</Link>
+                <Link to="/about" className='footer__link'>About</Link>
+                <Link to="/shop" className='footer__link'>Shop</Link>
+                <Link to="/login" className='footer__link'>Login</Link>
+              </div>
+            </li>
+
+            {/* 3-Ustun: Links */}
+            <li className='footer-item'>
+              <h2 className='footer__subtitle'>Links</h2>
+              <div className="footer__links-group">
+                <Link to="/" className='footer__link'>Home</Link>
+                <Link to="/about" className='footer__link'>About</Link>
+                <Link to="/shop" className='footer__link'>Shop</Link>
+                <Link to="/login" className='footer__link'>Login</Link>
+              </div>
+            </li>
+
+            {/* 4-Ustun: Contact */}
+            <li className='footer-item'>
+              <h2 className='footer__subtitle'>Contact</h2>
+              <div className="footer__links-group">
+                <Link to="#" className='footer__link footer__link--contact'>
+                  <FaLocationArrow className='footer__icon' />
+                  <span>26985 Brighton Lane, Lake Forest, CA</span>
+                </Link>
+                <Link to="#" className='footer__link footer__link--contact'>
+                  <FaAt className='footer__icon' />
+                  <span>support@Flowers.com</span>
+                </Link>
+                <Link to="#" className='footer__link footer__link--contact'>
+                  <FaPhone className='footer__icon' />
+                  <span>+1 236 5489</span>
+                </Link>
+              </div>
+            </li>
+
+          </ul>
         </div>
-      </footer>
-    </>
+      </div>
+    </footer>
   )
 }
 
