27db6492ab2a2c98d217454a545100610f8b589f
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-23 18:33:03 +0500

    comga style berdim

--- src/Components/Home/Com/Com.jsx
+++ src/Components/Home/Com/Com.jsx
@@ -11,34 +11,40 @@ const Com = () => {
     <div className="com">
         <div className="container">
             <div className="com__container">
-                <h1 className='com__title'></h1>
+                <h1 className='com__title'>Comments</h1>
                 <ul className='com__list'>
                     <li className='com__item'>
                         <img className='com__avatar' src={com1} alt="surat" />
-                        <p className='com__text'></p>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon1'/>
+                        <p className='com__text'>i get flowers from them for my baby mama, she love them so much 🔥💯</p>
+                        <li className='com__item2'>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon1'/>
+                        </li>
                     </li>
                     <li className='com__item'>
                         <img className='com__avatar' src={com2} alt="surat" />
-                        <p className='com__text'></p>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
+                        <p className='com__text'>i get flowers from them for my baby mama, she love them so much 🔥💯</p>
+                        <li className='com__item2'>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                        </li>
                     </li>
                     <li className='com__item'>
                         <img className='com__avatar' src={com3} alt="surat" />
-                        <p className='com__text'></p>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
-                        <FaStar className='com__icon'/>
+                        <p className='com__text'>i get flowers from them for my baby mama, she love them so much 🔥💯</p>
+                        <li className='com__item2'>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                            <FaStar className='com__icon'/>
+                        </li>
                     </li>
                 </ul>
             </div>
