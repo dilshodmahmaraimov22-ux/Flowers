c7692462ebee5292e238b738347a2009c6ea713e
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-24 15:52:35 +0500

    cards qoshilid

--- src/Components/Mode/Banner/Banner.jsx
+++ src/Components/Mode/Banner/Banner.jsx
@@ -2,7 +2,46 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <>
+    <div className="banner">
+        <div className="container">
+            <div className="banner__container">
+                <ul className='banner__list'>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                    <li className='banner__item'>
+                        <img className='banner__img' src="" alt="surat" />
+                        <h1 className='banner__title'></h1>
+                        <p className='banner__text'></p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
