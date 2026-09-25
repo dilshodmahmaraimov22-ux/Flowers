667848976594b34d6d745b4d8f2d472abc55147b
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-25 20:50:19 +0500

    counter qoshildi

--- src/Components/Cart/Cart/Cart.jsx
+++ src/Components/Cart/Cart/Cart.jsx
@@ -1,24 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Cart.css'
 import cart1 from '../../Images/cart1.jpg'
 
 const Cart = () => {
+    const [count, setCount] = useState(0);
     
   return (
     <>
     <div className="cart">
         <div className="container">
             <div className="cart__container">
-                <h1 className='cart__title'></h1>
+                <h1 className='cart__title'>Your Cart</h1>
                 <ul className='cart__list'>
                     <li className='cart__item'>
                         <img className='cart__img' src={cart1} alt="" />
-                        <h1 className='cart__name'></h1>
-                        <p className='cart__text'></p>
+                        <h1 className='cart__name'>Sun flower</h1>
+                        <p className='cart__text'>unit pric 10$</p>
                         <li className='cart__item2'>
-                            <button className='cart__btn'></button>
-                            <h1 className='cart__count'>0</h1>
-                            <button className='cart__btn'></button>
+                            <button onClick={()=>setCount(count + 1)} className='cart__btn'>+</button>
+                            <h1 className='cart__count'>{count}</h1>
+                            <button onClick={()=>setCount(count - 1)} className='cart__btn'>-</button>
                         </li>
                     </li>
                 </ul>
