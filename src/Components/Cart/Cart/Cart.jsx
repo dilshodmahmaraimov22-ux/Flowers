6399a6d40d340fd6aff00580fa3147074fc8a8db
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-25 20:37:33 +0500

    cards qoshildi

--- src/Components/Cart/Cart/Cart.jsx
+++ src/Components/Cart/Cart/Cart.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import './Cart.css'
+import cart1 from '../../Images/cart1.jpg'
 
 const Cart = () => {
+    
   return (
-    <div>Cart</div>
+    <>
+    <div className="cart">
+        <div className="container">
+            <div className="cart__container">
+                <h1 className='cart__title'></h1>
+                <ul className='cart__list'>
+                    <li className='cart__item'>
+                        <img className='cart__img' src={cart1} alt="" />
+                        <h1 className='cart__name'></h1>
+                        <p className='cart__text'></p>
+                        <li className='cart__item2'>
+                            <button className='cart__btn'></button>
+                            <h1 className='cart__count'>0</h1>
+                            <button className='cart__btn'></button>
+                        </li>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </>
   )
 }
 
