529e44b4054a697b26de3bb039e47037f75aaa3a
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-25 23:50:21 +0500

    tugadi

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import './Login.css'
 import { Link, useNavigate } from 'react-router-dom'
 import { FaHome } from 'react-icons/fa'
+import flower from './flower.jpg'
 
 
 const Login = () => {
@@ -50,12 +51,17 @@ const Login = () => {
                             />
                             <input className='login__password' 
                             type="password"
+                            placeholder='parolni kiriting'
                             value={password}
                             onChange={(e)=>setPassword(e.target.value)}
                              />
                             <input type="checkbox" />
                             <button type='submit' className='login__btn'>Login</button>
                         </form>
+                        <p className='login__textt'>dont have an account? Register</p>
+                    </li>
+                    <li className='login__img'>
+                        <img className='login__imgg' src={flower} alt="surat" />
                     </li>
                 </ul>
             </div>
