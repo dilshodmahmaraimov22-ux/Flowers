6adf1d0b394d79dad660080f5d85a7106850e3eb
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-25 20:18:08 +0500

    matn qoshildi

--- src/Components/Mode/Banner/Banner.jsx
+++ src/Components/Mode/Banner/Banner.jsx
@@ -15,33 +15,33 @@ const Banner = () => {
                 <ul className='banner__list'>
                     <li className='banner__item'>
                         <FaLightbulb/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Instant Server Start</h1>
+                        <p className='banner__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores, doloribus.</p>
                     </li>
                     <li className='banner__item'>
                         <FaBolt/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Lightning Fast HMR</h1>
+                        <p className='banner__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, consectetur!</p>
                     </li>
                     <li className='banner__item'>
                         <FaHammer/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Rich Features</h1>
+                        <p className='banner__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, facilis.</p>
                     </li>
                     <li className='banner__item'>
                         <FaBox/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Optimized Build</h1>
+                        <p className='banner__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, facere!</p>
                     </li>
                     <li className='banner__item'>
                         <FaScrewdriverWrench/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Universal Plugins</h1>
+                        <p className='banner__text'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Omnis, tempora!</p>
                     </li>
                     <li className='banner__item'>
                         <FaKey/>
-                        <h1 className='banner__title'></h1>
-                        <p className='banner__text'></p>
+                        <h1 className='banner__title'>Fully Typed APIs</h1>
+                        <p className='banner__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, beatae.</p>
                     </li>
                 </ul>
             </div>
