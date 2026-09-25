59c6d571e4cb2b156257f8e9500b79415123e8a3
Dilshod Mahmarayimo <dilshodmahmaraimov22@gmail.com>
2026-09-25 21:50:22 +0500

    hatolar tog'irlandi

--- src/Components/Cart/Cart/Cart.jsx
+++ src/Components/Cart/Cart/Cart.jsx
@@ -16,37 +16,36 @@ const Cart = () => {
             <div className="cart__container">
                 <h1 className='cart__title'>Your Cart</h1>
                 <ul className='cart__list'>
-                    <li className='cart__item'>
-                        <img className='cart__img' src={cart1} alt="" />
-                    </li>
-                    <li className='cart__item'>
-                        <h1 className='cart__name'>Sun flower</h1>
+                        <li className='cart__1'>
+                            <img className='cart__img' src={cart1} alt="" />
+                        </li>
+                        <li className='cart__1'>
+                            <h1 className='cart__name'>Sun flower</h1>
                         <p className='cart__text'>unit pric 10$</p>
                         <button onClick={()=>setCount(count + 1)} className='cart__btn'>+</button>
                         <h1 className='cart__count'>{count}</h1>
                         <button onClick={()=>setCount(count - 1)} className='cart__btn'>-</button>
-                    </li>
-                    <li className='cart__item'>
-                        <button className='cart__trash'><FaTrash/></button>
+                        </li>
+                        <li className='cart__1'>
+                            <button className='cart__trash'><FaTrash/></button>
                         <h1 className='cart__total'>Total 10$</h1>
-                    </li>
+                        </li>
 
-                    <li className='cart__items'>
-                        <img className='cart__img' src={cart2} alt="" />
-                    </li>
-                    <li className='cart__items'>
-                        <h1 className='cart__name'>White flower</h1>
+                        <li className='cart__2'>
+                            <img className='cart__img' src={cart2} alt="" />
+                        </li>
+                        <li className='cart__2'>
+                            <h1 className='cart__name'>White flower</h1>
                         <p className='cart__text'>unit pric 10$</p>
                         <button onClick={()=>setSount(sount + 1)} className='cart__btn'>+</button>
                         <h1 className='cart__count'>{sount}</h1>
                         <button onClick={()=>setSount(sount - 1)} className='cart__btn'>-</button>
-                    </li>
-                    <li className='cart__items'>
-                        <button className='cart__trash'><FaTrash/></button>
-                        <h1 className='cart__total'>Total 10$</h1>
-                    </li>
-                </ul>
-                <ul className='cart__list'>
+                        </li>
+                        <li className='cart__2'>
+                            <button className='cart__trash'><FaTrash/></button>
+                            <h1 className='cart__total'>Total 10$</h1>
+                        </li>
+
                     <li className='cart__item2'>
                         <h1 className='cart__title2'>Subtotal for 3 items: 62$</h1>
                         <button className='cart__check'>Checkout</button>
